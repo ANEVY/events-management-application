db60b85984869d8201dd086f3d5629e957c9d582
Akombo Neville Akwo <akomboneville55@gmail.com>
2024-01-07 18:13:39 +0100

    Authorization using policies

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Models\Attendee;
 use App\Models\Event;
+use App\Policies\AttendeePolicy;
+use App\Policies\EventPolicy;
 use Illuminate\Support\Facades\Gate;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -16,7 +18,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+        // Event::class => EventPolicy::class,
+        // Attendee::class => AttendeePolicy::class
     ];
 
     /**
@@ -25,14 +28,14 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        Gate::define('update-event', function($user, Event $event){
-            return $user->id == $event->user_id;
-        });
-        
-        Gate::define('delete-attendee', function($user, Event $event, Attendee $attendee){
-            return ($user->id == $event->user_id) || 
-                ($user->id == $attendee->user_id) ;
-        });
+        // Gate::define('update-event', function($user, Event $event){
+        //     return $user->id == $event->user_id;
+        // });
+
+        // Gate::define('delete-attendee', function($user, Event $event, Attendee $attendee){
+        //     return ($user->id == $event->user_id) || 
+        //         ($user->id == $attendee->user_id) ;
+        // });
         
     }
 }
